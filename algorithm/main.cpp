9bb22bc598b887717a77097d4c44bc562de67599
Konnase Lee <lqp19940918@163.com>
2019-03-07 12:14:43 +0800

    Initial Commit

---
+++ algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by 李青坪 on 2019/3/7.
+//  Copyright © 2019 李青坪. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
